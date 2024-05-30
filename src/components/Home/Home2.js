5cce51140e7cb67f5e3cdc505795ba72184a117f
Savan Fami <savanfami1239@gmail.com>
2024-05-30 22:58:24 +0530

    initial commit

---
+++ src/components/Home/Home2.js
@@ -0,0 +1,93 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import myImg from "../../Assets/avatar.svg";
+import Tilt from "react-parallax-tilt";
+import {
+  SiLeetcode,
+  AiFillGithub,
+  
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Home2() {
+  return (
+    <Container fluid className="home-about-section" id="about">
+      <Container>
+        <Row>
+          <Col md={8} className="home-about-description">
+            <h1 style={{ fontSize: "2.6em" }}>
+              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+            </h1>
+            <p className="home-about-body">
+              
+
+
+As a passionate software developer deeply entrenched in the realms of JavaScript, React, I embark on a journey fueled by the relentless pursuit of knowledge and innovation. With a fervent dedication to my craft, I am not merely a seeker of solutions but a cultivator of elegant and efficient code.
+              <br />
+             
+           
+           
+            </p>
+          </Col>
+          <Col md={4} className="myAvtar">
+            <Tilt>
+              <img src={myImg} className="img-fluid" alt="avatar" />
+            </Tilt>
+          </Col>
+        </Row>
+        <Row>
+          <Col md={12} className="home-about-social">
+            <h1>FIND ME ON</h1>
+            <p>
+              Feel free to <span className="purple">connect </span>with me
+            </p>
+            <ul className="home-about-social-links">
+              <li className="social-icons">
+                <a
+                  href="https://github.com/savanfami"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <AiFillGithub />
+                </a>
+              </li>
+             
+              <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/savan-fami/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <FaLinkedinIn />
+                </a>
+              </li>
+              {/* <li className="social-icons">
+                <a
+                  href="https://www.linkedin.com/in/savan-fami/"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour  home-social-icons"
+                >
+                  <SiLeetcode />
+                </a>
+              </li> */}
+              {/* <li className="social-icons">
+                <a
+                  href="https://www.instagram.com/soumyajit4419"
+                  target="_blank"
+                  rel="noreferrer"
+                  className="icon-colour home-social-icons"
+                >
+                  <AiFillInstagram />
+                </a>
+              </li> */}
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+export default Home2;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import ProjectCard from "./ProjectCards";
+import Particle from "../Particle";
+
+import shoex from '../../Assets/Projects/Screenshot 2024-05-27 052836.png'
+import crud from '../../Assets/Projects/Screenshot 2024-04-28 180854.png'
+import netflix from '../../Assets/Projects/Screenshot 2024-04-28 193555.png'
+import olx from '../../Assets/Projects/Screenshot 2024-04-28 175300.png'
+
+
+function Projects() {
+  return (
+    <Container fluid className="project-section">
+      <Particle />
+      <Container>
+        <h1 className="project-heading">
+          My Recent <strong className="purple">Works </strong>
+        </h1>
+        <p style={{ color: "white" }}>
+          Here are a few projects I've worked on recently.
+        </p>
+        <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={shoex}
+              isBlog={false}
+              title="Shoex"
+              description="This is my first node.js project.Shoex is an E-commerce web application, especially for multibrand unisex sneakers where users can view products and buy them. It has almost every feature and function that are required for an E-commerce app."
+              ghLink="https://github.com/savanfami/shoex_ecommerce"
+              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={netflix}
+              isBlog={false}
+              title="Netflix Clone"
+              description="This project is a clone of the Netflix website, built with JavaScript, React, React-Vite, Tailwind CSS, Firebase, and API integration. It replicates the Netflix user experience, offering a responsive and dynamic platform for streaming content."
+              ghLink="https://github.com/savanfami/netflix-Clone-react"
+
+              // demoLink="https://blogs.soumya-jit.tech/"
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={olx}
+              isBlog={false}
+              title="Olx Clone"
+              description="Implemented with React, Firebase, Tailwind CSS, and React-Vite, this OLX website clone offers a seamless user experience. Built with JavaScript, it replicates OLX's functionality, enabling users to buy and sell products effortlessly."
+              ghLink="https://github.com/savanfami/Olx-clone-React"
+              demoLink="https://editor.soumya-jit.tech/"              
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={crud}
+              isBlog={false}
+              title="React Crud App"
+              description="This CRUD application utilizes MongoDB, React, Vite, Tailwind CSS, Node.js, and Express. It features Cloudinary integration for profile image uploading. Users can create, read, update, and delete data seamlessly across the platform."
+              ghLink="https://github.com/soumyajit4419/Plant_AI"
+              demoLink="https://plant49-ai.herokuapp.com/"
+            />
+          </Col>
+
+         
+
+          
+        </Row>
+      </Container>
+    </Container>
+  );
+}
+
+export default Projects;
