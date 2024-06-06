e6ebc7f7504a9a1cc1589fc8b428f9e3e6750243
Savan Fami <savanfami1239@gmail.com>
2024-06-06 10:12:32 +0530

    made new changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -82,25 +82,8 @@ function NavBar() {
               </Nav.Link>
             </Nav.Item>
 
-            <Nav.Item>
-              <Nav.Link
-                as={Link}
-                to="/resume"
-                onClick={() => updateExpanded(false)}
-              >
-                <CgFileDocument style={{ marginBottom: "2px" }} /> Resume
-              </Nav.Link>
-            </Nav.Item>
-
-            <Nav.Item>
-              <Nav.Link
-                href="https://soumyajitblogs.vercel.app/"
-                target="_blank"
-                rel="noreferrer"
-              >
-                <ImBlog style={{ marginBottom: "2px" }} /> Blogs
-              </Nav.Link>
-            </Nav.Item>
+           
+          
             <Nav.Item className="fork-btn">
               <Button
                 href="https://github.com/savanfami/my-portfolio"
