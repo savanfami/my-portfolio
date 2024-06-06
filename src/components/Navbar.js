e48a9dd08282625c49ccc11b1882e617f2ef0fe3
Savan Fami <savanfami1239@gmail.com>
2024-06-06 09:36:05 +0530

    made new changes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -101,17 +101,18 @@ function NavBar() {
                 <ImBlog style={{ marginBottom: "2px" }} /> Blogs
               </Nav.Link>
             </Nav.Item>
-
-            {/* <Nav.Item className="fork-btn">
+            <Nav.Item className="fork-btn">
               <Button
-                href="https://github.com/soumyajit4419/Portfolio"
+                href="https://github.com/savanfami/my-portfolio"
                 target="_blank"
                 className="fork-btn-inner"
               >
                 <CgGitFork style={{ fontSize: "1.2em" }} />{" "}
                 <AiFillStar style={{ fontSize: "1.1em" }} />
               </Button>
-            </Nav.Item> */}
+            </Nav.Item>
+
+          
           </Nav>
         </Navbar.Collapse>
       </Container>
